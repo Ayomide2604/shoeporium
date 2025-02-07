7150cea14e11a48f08f28e2a2271e968d53dfe7c
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-02-07 07:57:41 +0100

    updated render database

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -87,18 +87,18 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
-# DATABASE_URL = os.getenv('DATABASE_URL')
-
-# DATABASES = {
-#     'default': dj_database_url.config(default=DATABASE_URL)
-# }
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=DATABASE_URL)
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
